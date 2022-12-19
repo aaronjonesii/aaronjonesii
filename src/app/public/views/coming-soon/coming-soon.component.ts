cdfa99eba97946124f368b434a559768a620a99b
Anonymous One <jonesaa5@msu.edu>
2022-12-19 02:49:53 -0500

    chore: sticky footer

--- src/app/public/views/coming-soon/coming-soon.component.ts
+++ src/app/public/views/coming-soon/coming-soon.component.ts
@@ -7,4 +7,5 @@ import { Component } from '@angular/core';
 })
 export class ComingSoonComponent {
   public readonly name = 'Aaron Jones II';
+  public readonly currentYear = new Date().getFullYear();
 }
