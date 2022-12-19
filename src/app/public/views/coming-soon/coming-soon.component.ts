8cbbacfd197ad8e3ab80ffccbaa9078e938f7d73
Anonymous One <jonesaa5@msu.edu>
2022-12-19 17:17:40 -0500

    feat: update subscribe

--- src/app/public/views/coming-soon/coming-soon.component.ts
+++ src/app/public/views/coming-soon/coming-soon.component.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
 import { nav_path } from "../../../app-routing.module";
+import { FormControl, FormGroup, Validators } from "@angular/forms";
+import { ConsoleLoggerService } from "../../../core/services/console-logger.service";
 
 @Component({
   selector: 'aj-coming-soon',
@@ -11,4 +13,32 @@ export class ComingSoonComponent {
   public readonly name = 'Aaron Jones II';
   public readonly currentYear = new Date().getFullYear();
   public readonly github = 'https://github.com/aaronjonesii';
+  public subscribeForm = new FormGroup({
+    email: new FormControl<string>(
+      '',
+      { nonNullable:true, validators: [Validators.required, Validators.email ] }
+    )
+  });
+  public loading = false;
+
+  constructor(private cLog: ConsoleLoggerService) {}
+  get email() { return this.subscribeForm.controls.email; }
+
+  public onSubscribe(): void {
+    if (this.subscribeForm.invalid) {
+      this.cLog.warn(`Please enter a valid email address.`);
+      return;
+    }
+
+    this.loading = true;
+    this.subscribeForm.disable();
+
+    const email = this.email.value;
+    this.cLog.openSnackBar(`subscribe email: '${email}'`, '🆗', {
+      duration: 0
+    });
+
+    this.subscribeForm.enable();
+    this.loading = false;
+  }
 }
--- src/app/public/views/coming-soon/coming-soon.module.ts
+++ src/app/public/views/coming-soon/coming-soon.module.ts
@@ -6,13 +6,14 @@ import { MatButtonModule } from "@angular/material/button";
 import { MatIconModule } from "@angular/material/icon";
 import { MatFormFieldModule } from "@angular/material/form-field";
 import { MatInputModule } from "@angular/material/input";
+import { ReactiveFormsModule } from "@angular/forms";
 
 const ANGULAR_MATERIAL_MODULES = [
   MatButtonModule, MatIconModule,
   MatFormFieldModule, MatInputModule
 ];
 
-const CORE_MODULES = [CommonModule];
+const CORE_MODULES = [CommonModule, ReactiveFormsModule];
 
 const COMPONENTS = [ComingSoonComponent];
 
