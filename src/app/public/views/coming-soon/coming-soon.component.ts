87d9eab3f5f35b0f96bfaefe00ae427586522095
Anonymous One <jonesaa5@msu.edu>
2022-12-22 22:36:06 -0500

    coming-soon: use seo service
    Use SEO service to set page title

--- src/app/public/views/coming-soon/coming-soon.component.ts
+++ src/app/public/views/coming-soon/coming-soon.component.ts
@@ -3,6 +3,7 @@ import { nav_path } from "../../../app-routing.module";
 import { FormControl, FormGroup, Validators } from "@angular/forms";
 import { ConsoleLoggerService } from "../../../core/services/console-logger.service";
 import { FunctionsService } from "../../../shared/services/functions.service";
+import { SeoService } from "../../../core/services/seo.service";
 
 @Component({
   selector: 'aj-coming-soon',
@@ -25,8 +26,14 @@ export class ComingSoonComponent {
 
   constructor(
     private cLog: ConsoleLoggerService,
-    private fun: FunctionsService
-  ) {}
+    private fun: FunctionsService,
+    private seo: SeoService
+  ) {
+    this.seo.generateTags({
+      title: 'Coming Soon page',
+      route: nav_path.comingSoon
+    });
+  }
   get email() { return this.subscribeForm.controls.email; }
 
   public async onSubscribe(): Promise<void> {
