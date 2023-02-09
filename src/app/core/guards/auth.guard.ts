4dafb2f337d62d18cf8b065226a60a81417c711f
Anonymous One <jonesaa5@msu.edu>
2023-02-08 20:58:04 -0500

    feat: authentication guard

---
+++ src/app/core/guards/auth.guard.ts
@@ -0,0 +1,29 @@
+import { Injectable } from '@angular/core';
+import { ActivatedRouteSnapshot, CanActivate, Router, RouterStateSnapshot } from '@angular/router';
+import { map, Observable, take, tap } from 'rxjs';
+import { AuthService } from "../services/auth.service";
+import { nav_path } from "../../app-routing.module";
+
+@Injectable({ providedIn: 'root' })
+export class AuthGuard implements CanActivate {
+
+  constructor(
+    private router: Router,
+    private auth: AuthService
+  ) {}
+
+  canActivate(
+    route: ActivatedRouteSnapshot,
+    state: RouterStateSnapshot): Observable<boolean> {
+    return this.auth.loadUser.pipe(
+      take(1),
+      map(user => !!user),
+      tap(isAuthenticated => {
+        if (!isAuthenticated) {
+          this.router.navigate([ nav_path.signIn ], { queryParams: { "redirectURL": state.url } });
+        }
+      })
+    );
+  }
+
+}
