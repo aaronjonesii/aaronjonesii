5e31351e1825d6f67f28e3f273a9e3b5081036df
Anonymous One <jonesaa5@msu.edu>
2023-02-08 20:58:28 -0500

    feat: administrator guard

---
+++ src/app/core/guards/admin.guard.ts
@@ -0,0 +1,31 @@
+import { Injectable } from '@angular/core';
+import { ActivatedRouteSnapshot, CanActivate, Router, RouterStateSnapshot } from '@angular/router';
+import { from, Observable, of } from 'rxjs';
+import { map, switchMap, take, tap } from 'rxjs/operators';
+import { nav_path } from '../../app-routing.module';
+import { AuthService } from '../services/auth.service';
+
+@Injectable({ providedIn: 'root', })
+export class AdminGuard implements CanActivate {
+  constructor(
+    private auth: AuthService,
+    private router: Router
+  ) {}
+  canActivate(next: ActivatedRouteSnapshot, state: RouterStateSnapshot):
+    Observable<boolean> {
+    return this.auth.loadUser.pipe(
+      take(1),
+      switchMap(user => {
+        if (user) {
+          return from(this.auth.loadUserToken(user))
+            .pipe(
+              map(idTokenResult => !!idTokenResult?.claims['admin']),
+              tap((isAdmin) => { if (!isAdmin) this.router.navigate([nav_path.forbidden])})
+            )
+        } else {
+          this.router.navigate([nav_path.signIn], { queryParams:{'redirectURL':state.url} });
+          return of(false); }
+      })
+    )
+  }
+}
