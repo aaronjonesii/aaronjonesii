50b16e8c9bae55e25bb8b70d86ef748022267abe
Anonymous One <jonesaa5@msu.edu>
2023-02-10 04:21:36 -0500

    chore: admin route

--- src/app/private/private.module.ts
+++ src/app/private/private.module.ts
@@ -1,9 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { admin_nav_path } from "./views/admin/admin.module";
+import { AdminGuard } from "../core/guards/admin.guard";
 
-export const private_nav_path = {};
+export const private_nav_path = {
+  admin: '/admin', ...admin_nav_path
+};
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'admin', loadChildren: () => import('./views/admin/admin.module').then(m => m.AdminModule),
+    canActivate: [AdminGuard] }
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
