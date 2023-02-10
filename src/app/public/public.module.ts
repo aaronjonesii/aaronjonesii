e4b6602a1a2db08f6f62dc055e5673080ff90250
Anonymous One <jonesaa5@msu.edu>
2023-02-09 19:41:16 -0500

    chore: add auth and poilicies

--- src/app/public/public.module.ts
+++ src/app/public/public.module.ts
@@ -1,14 +1,20 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { home_nav_path } from './views/home/home.module';
+import { auth_nav_path } from "./views/auth/auth.module";
+import { policies_nav_path } from "./views/policies/policies.module";
 
 export const public_nav_path = {
   home: '/', ...home_nav_path,
-  comingSoon: '/coming-soon'
+  comingSoon: '/coming-soon',
+  auth: '/auth', ...auth_nav_path,
+  policies: '/policies', ...policies_nav_path
 };
 
 const routes: Routes = [
-  { path: '', loadChildren: () => import('./views/home/home.module').then(m => m.HomeModule) }
+  { path: '', loadChildren: () => import('./views/home/home.module').then(m => m.HomeModule) },
+  { path: 'auth', loadChildren: () => import('./views/auth/auth.module').then(m => m.AuthModule) },
+  { path: 'policies', loadChildren: () => import('./views/policies/policies.module').then(m => m.PoliciesModule) }
 ];
 
 @NgModule({
