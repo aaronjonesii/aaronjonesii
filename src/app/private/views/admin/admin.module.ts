8726e826db32810b216a07af8bf2e2d2fe33324b
Anonymous One <jonesaa5@msu.edu>
2023-02-10 21:27:38 -0500

    chore: admin projects route

--- src/app/private/views/admin/admin.module.ts
+++ src/app/private/views/admin/admin.module.ts
@@ -7,23 +7,34 @@ import { NavigationBarModule } from "../../../shared/components/navigation-bar/n
 import { MatToolbarModule } from "@angular/material/toolbar";
 import { MatButtonModule } from "@angular/material/button";
 import { MasonryCardsModule } from "../../../shared/components/masonry-cards/masonry-cards.module";
+import { AdminProjectsComponent } from './views/admin-projects/admin-projects.component';
+import { ConfirmDialogModule } from "../../../shared/components/confirm-dialog/confirm-dialog.module";
+import { MatMenuModule } from "@angular/material/menu";
+import { AdminProjectsGridModule } from "./shared/admin-projects-grid/admin-projects-grid.module";
+import { MatIconModule } from "@angular/material/icon";
 
 const ANGULAR_MATERIAL_MODULES = [
-  MatToolbarModule, MatButtonModule
+  MatToolbarModule, MatButtonModule, MatMenuModule,
+  MatIconModule
 ];
 
 const CORE_MODULES = [CommonModule];
 
-const COMPONENTS = [AdminComponent, AdminDashboardComponent];
+const COMPONENTS = [
+  AdminComponent, AdminDashboardComponent,
+  AdminProjectsComponent
+];
 
 export const admin_nav_path = {
-  adminDashboard: '/admin/dashboard'
+  adminDashboard: '/admin/dashboard',
+  adminProjects: '/admin/projects'
 };
 
 const routes: Routes = [
   { path: '', component: AdminComponent, children: [
       { path: '', redirectTo: 'dashboard', pathMatch: 'full' },
       { path: 'dashboard', component: AdminDashboardComponent },
+      { path: 'projects', component: AdminProjectsComponent }
     ] }
 ];
 
@@ -32,7 +43,7 @@ const routes: Routes = [
   imports: [
     RouterModule.forChild(routes), ...CORE_MODULES,
     ...ANGULAR_MATERIAL_MODULES, NavigationBarModule,
-    MasonryCardsModule
+    MasonryCardsModule, ConfirmDialogModule, AdminProjectsGridModule
   ],
   exports: [...COMPONENTS]
 })
