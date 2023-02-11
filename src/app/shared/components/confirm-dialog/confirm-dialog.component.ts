a3b62201476c64d44f6d17cd0e6568a105fc8e47
Anonymous One <jonesaa5@msu.edu>
2023-02-10 21:25:18 -0500

    feat: global reusable confirm dialog component

---
+++ src/app/shared/components/confirm-dialog/confirm-dialog.component.ts
@@ -0,0 +1,16 @@
+import { Component, Inject } from '@angular/core';
+import { MAT_DIALOG_DATA } from "@angular/material/dialog";
+
+@Component({
+  selector: 'bhb-confirm-dialog',
+  templateUrl: './confirm-dialog.component.html',
+  styleUrls: ['./confirm-dialog.component.scss']
+})
+export class ConfirmDialogComponent {
+  public title = 'Are you sure?';
+  public description = 'This action is irreversible, be careful.';
+  constructor(@Inject(MAT_DIALOG_DATA) public data: { title: string, description: string }) {
+    this.title = data.title;
+    this.description = data.description;
+  }
+}
