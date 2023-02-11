23534c855c169fabf56809965b7ee283f8f838a5
Anonymous One <jonesaa5@msu.edu>
2023-02-10 21:24:23 -0500

    chore: add admin navigation bar

--- src/app/shared/services/menu.service.ts
+++ src/app/shared/services/menu.service.ts
@@ -11,11 +11,19 @@ export class MenuService {
     // { name: 'Contact', icon: 'contact_mail', routerLink: ['/contact'] }
   ];
 
+  public readonly adminNavigationBarMenu = [
+    { name: 'Dashboard', icon: 'dashboard', routerLink: [nav_path.adminDashboard] },
+    { name: 'Projects', icon: 'supervised_user_circle', routerLink: ['/admin/projects'] },
+    // { name: 'Comments', icon: 'work_history', routerLink: ['/admin/comments'] },
+    // { name: 'Users', icon: 'contact_mail', routerLink: ['/admin/users'] },
+    { name: 'Home', icon: 'home', routerLink: [nav_path.home] }
+  ];
+
   public readonly adminMenu: GenericItem[] = [
     { name: 'Dashboard', icon: 'space_dashboard', routerLink: [nav_path.admin],
       description: 'Manage admin components throughout this website.' },
-    // { name: 'Projects', description: 'Create and manage the projects displayed here.',
-    //   routerLink: ['/admin/projects'], tooltip: 'Products', icon: 'work_history' },
+    { name: 'Projects', description: 'Create and manage the projects displayed here.',
+      routerLink: ['/admin/projects'], tooltip: 'Products', icon: 'work_history' },
     // { name: 'Comments', icon: 'comment', routerLink: ['/admin/comments'],
     //   description: 'Manage story comments.' },
     // { name: 'Customers', description: 'Manage this websites customers.',
