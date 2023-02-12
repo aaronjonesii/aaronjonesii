b257f0983f3e7e14fcd8155b1091766f61d0b1dc
Anonymous One <jonesaa5@msu.edu>
2023-02-12 16:18:23 -0500

    chore: update project interface

--- src/app/shared/interfaces/project.ts
+++ src/app/shared/interfaces/project.ts
@@ -1,18 +1,28 @@
-import { FieldValue } from "@angular/fire/firestore";
+import { firebaseTimestamp } from "./timestamp";
+import { FieldValue } from "@angular/fire/firestore/firebase";
 
 export interface Project {
-  id: string,
+  id?: string,
   name: string,
   description: string,
-  content: string,
-  image?: string,
-  tags?: string[],
-  livePreviewLink?: string,
-  sourceCodeLink?: string,
-  status: "draft" | "archived" | "published",
-  visibility: "public" | "private",
-  created: FieldValue,
-  updated?: FieldValue,
+  content: string | null,
+  image: string | null,
+  tags: string[] | null,
+  livePreviewLink: string | null,
+  sourceCodeLink: string | null,
+  status: ProjectStatusType,
+  visibility: ProjectVisibilityType,
+  created: firebaseTimestamp,
+  updated: firebaseTimestamp | null,
   featured: boolean,
   allowComments: boolean
 }
+
+export type ProjectStatusType = "draft" | "archived" | "published";
+export const ProjectStatuses = ["draft", "archived", "published"];
+
+export type ProjectVisibilityType = "public" | "private";
+export const ProjectVisibilities = ["public", "private"];
+
+export interface ProjectWithID extends Project { id: string; }
+export interface WriteProject extends Project { created: FieldValue, updated: FieldValue | null }
