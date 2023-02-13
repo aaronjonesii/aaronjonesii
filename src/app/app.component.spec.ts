1b0e29d7cfdb4e7be6d61515f2c8e4b6506348cb
Anonymous One <jonesaa5@msu.edu>
2023-02-12 19:37:12 -0500

    feat: unit testing

---
+++ src/app/app.component.spec.ts
@@ -0,0 +1,36 @@
+import { inject, TestBed, waitForAsync } from '@angular/core/testing';
+import { AppComponent, GITHUB_ICON_SVG } from './app.component';
+import { MatIcon, MatIconRegistry } from "@angular/material/icon";
+import { DomSanitizer } from "@angular/platform-browser";
+import { RouterTestingModule } from "@angular/router/testing";
+import { MatIconTestingModule } from "@angular/material/icon/testing";
+
+describe('AppComponent', () => {
+
+  beforeEach(waitForAsync(() => {
+    TestBed.configureTestingModule({
+      imports: [RouterTestingModule, MatIconTestingModule],
+      declarations: [AppComponent, MatIcon]
+    }).compileComponents();
+  }));
+
+  beforeEach(inject(
+    [MatIconRegistry, DomSanitizer],
+    (iconRegistry: MatIconRegistry, sanitizer: DomSanitizer) => {
+      iconRegistry.addSvgIconLiteral("github", sanitizer.bypassSecurityTrustHtml(GITHUB_ICON_SVG));
+    }
+  ));
+
+  it('should create the app', () => {
+    const fixture = TestBed.createComponent(AppComponent);
+    const app = fixture.componentInstance;
+    expect(app).toBeTruthy();
+  });
+
+  it('should have a router-outlet directive', () => {
+    const fixture = TestBed.createComponent(AppComponent);
+    fixture.detectChanges();
+    const compiled = fixture.debugElement.nativeElement;
+    expect(compiled.querySelector('router-outlet')).toBeTruthy();
+  });
+});
