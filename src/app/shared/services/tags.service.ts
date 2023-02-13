b376813772f69b9b70e2221d02e7bdeab6cfee2b
Anonymous One <jonesaa5@msu.edu>
2023-02-12 19:37:21 -0500

    feat: tags service

---
+++ src/app/shared/services/tags.service.ts
@@ -0,0 +1,14 @@
+import { Injectable } from '@angular/core';
+
+@Injectable({ providedIn: 'root' })
+export class TagsService {
+
+  constructor() { }
+
+  public removedTags(previousTags: string[], currentTags: string[]): string[] {
+    return previousTags.filter(oldTag => !currentTags.includes(oldTag));
+  }
+  public addedTags(previousTags: string[], currentTags: string[]): string[] {
+    return currentTags.filter(newTag => !previousTags.includes(newTag));
+  }
+}
