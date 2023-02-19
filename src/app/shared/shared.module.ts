ea20eaa06fdda3cd70ad471daa3e6e0856774936
Anonymous One <jonesaa5@msu.edu>
2023-02-18 20:35:26 -0500

    feat: reusable user photo component

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -4,12 +4,14 @@ import { SafePipe } from './pipes/safe.pipe';
 import { SlugifyPipe } from "./pipes/slugify.pipe";
 import { LoadingComponent } from "./components/loading/loading.component";
 import { LoadingOrErrorComponent } from "./components/loading-or-error/loading-or-error.component";
+import { UserPhotoComponent } from "./components/user-photo/user-photo.component";
+import { MatIconModule } from "@angular/material/icon";
 
-const ANGULAR_MATERIAL_MODULES = []!;
+const ANGULAR_MATERIAL_MODULES = [MatIconModule];
 
 const CORE_MODULES = [CommonModule];
 
-const COMPONENTS = [LoadingComponent, LoadingOrErrorComponent];
+const COMPONENTS = [LoadingComponent, LoadingOrErrorComponent, UserPhotoComponent];
 
 const PIPES = [SafePipe, SlugifyPipe];
 const DIRECTIVES = []!;
