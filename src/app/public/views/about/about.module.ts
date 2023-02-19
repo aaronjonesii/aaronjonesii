b4730936d79ee69278ec5bdf1491401b20824a67
Anonymous One <jonesaa5@msu.edu>
2023-02-19 02:06:30 -0500

    feat: about page

---
+++ src/app/public/views/about/about.module.ts
@@ -0,0 +1,21 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { RouterModule, Routes } from "@angular/router";
+import { AboutComponent } from "./about.component";
+
+const ANGULAR_MATERIAL_MODULES = []!;
+
+const CORE_MODULES = [CommonModule];
+
+const COMPONENTS = [AboutComponent];
+
+const routes: Routes = [
+  { path: '', component: AboutComponent, pathMatch: 'full' }
+];
+
+@NgModule({
+  declarations: [...COMPONENTS],
+  imports: [RouterModule.forChild(routes), ...CORE_MODULES, ...ANGULAR_MATERIAL_MODULES],
+  exports: [...COMPONENTS]
+})
+export class AboutModule { }
