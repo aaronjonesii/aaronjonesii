274cb3bc76b59f39f869d8a9f30612bc23708629
Anonymous One <jonesaa5@msu.edu>
2023-02-20 04:54:57 -0500

    chore: update user interface

--- src/app/shared/interfaces/user.ts
+++ src/app/shared/interfaces/user.ts
@@ -2,14 +2,18 @@ import { firebaseTimestamp, TimeStamp } from "./timestamp";
 
 export interface User {
   displayName?: string,
-  email?: string,
+  email: string,
   phoneNumber?: string,
   photoURL?: string,
   joined?: firebaseTimestamp,
   admin?: boolean,
   id?: string,
+  following?: boolean,
+  updated?: firebaseTimestamp,
 }
-
 export interface readUser extends User {
   joined?: TimeStamp
+}
+export interface UserWithID extends readUser {
+  id: string,
 }
