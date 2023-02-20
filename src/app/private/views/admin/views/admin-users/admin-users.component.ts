ae2dea69bdb907fd880cac5b37726e24760592ba
Anonymous One <jonesaa5@msu.edu>
2023-02-20 04:53:58 -0500

    feat: update user document

--- src/app/private/views/admin/views/admin-users/admin-users.component.ts
+++ src/app/private/views/admin/views/admin-users/admin-users.component.ts
@@ -5,6 +5,8 @@ import { FirebaseError } from '@angular/fire/app/firebase';
 import { readUser } from "../../../../../shared/interfaces/user";
 import { nav_path } from 'src/app/app-routing.module';
 import { FirestoreService } from "../../../../../shared/services/firestore.service";
+import { FunctionsService } from "../../../../../shared/services/functions.service";
+import { ConsoleLoggerService } from "../../../../../core/services/console-logger.service";
 
 @Component({
   selector: 'aj-admin-users',
@@ -12,12 +14,16 @@ import { FirestoreService } from "../../../../../shared/services/firestore.servi
   styleUrls: ['./admin-users.component.scss']
 })
 export class AdminUsersComponent {
-  public readonly title = 'Customers';
+  public readonly title = 'Users';
   public readonly nav_path = nav_path;
   public users$?: Observable<readUser[]>;
   public error?: FirebaseError;
 
-  constructor(private db: FirestoreService) {
+  constructor(
+    private db: FirestoreService,
+    private fn: FunctionsService,
+    private cLog: ConsoleLoggerService,
+  ) {
     this.users$ = (this.db.col$(`users`, { idField: 'id' }) as Observable<readUser[]>)
       .pipe(catchError((error: FirebaseError) => {
         console.error(`Something went wrong loading users`, error.message)
@@ -26,4 +32,10 @@ export class AdminUsersComponent {
       }))
   }
 
+  public async updateUser(user?: string) {
+    await this.fn.httpsCallable<{success: boolean, message: string}>
+    (`admin-updateUser`, {user: user})
+      .then(result => this.cLog.log(result.message))
+      .catch(error => this.cLog.error(error.message, error));
+  }
 }
