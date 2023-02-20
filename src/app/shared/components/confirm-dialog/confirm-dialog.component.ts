f44901e434e68413ba78b06a0f7a5231d57d28a2
Anonymous One <jonesaa5@msu.edu>
2023-02-20 04:52:23 -0500

    chore: update confirm dialog
    Made the title optional while also adding a button text option

--- src/app/shared/components/confirm-dialog/confirm-dialog.component.ts
+++ src/app/shared/components/confirm-dialog/confirm-dialog.component.ts
@@ -7,10 +7,12 @@ import { MAT_DIALOG_DATA } from "@angular/material/dialog";
   styleUrls: ['./confirm-dialog.component.scss']
 })
 export class ConfirmDialogComponent {
-  public title = 'Are you sure?';
+  public title?: string;
   public description = 'This action is irreversible, be careful.';
-  constructor(@Inject(MAT_DIALOG_DATA) public data: { title: string, description: string }) {
+  public buttonText = 'Confirm'
+  constructor(@Inject(MAT_DIALOG_DATA) public data: { title?: string, description: string, buttonText?: string }) {
     this.title = data.title;
     this.description = data.description;
+    if (data.buttonText) this.buttonText = data.buttonText;
   }
 }
