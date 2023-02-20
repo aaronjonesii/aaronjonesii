893e503c5b1d3077d0d17965e76b97256b84cbab
Anonymous One <jonesaa5@msu.edu>
2023-02-20 04:52:51 -0500

    chore: use enums

--- src/app/public/views/work/work.component.ts
+++ src/app/public/views/work/work.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 import { BehaviorSubject, catchError, map, Observable, switchMap, throwError } from "rxjs";
-import { Project } from "../../../shared/interfaces/project";
+import { Project, ProjectStatus, ProjectVisibility } from "../../../shared/interfaces/project";
 import { FirestoreService } from "../../../shared/services/firestore.service";
 import { ConsoleLoggerService } from "../../../core/services/console-logger.service";
 import { QueryConstraint, where } from "@angular/fire/firestore";
+import { nav_path } from 'src/app/app-routing.module';
 
 @Component({
   selector: 'aj-work',
@@ -11,6 +12,7 @@ import { QueryConstraint, where } from "@angular/fire/firestore";
   styleUrls: ['./work.component.scss']
 })
 export class WorkComponent {
+  public readonly nav_path = nav_path;
   public filterSubject = new BehaviorSubject<'all' | 'active' | 'inactive'>('all');
   public filter$ = this.filterSubject.asObservable();
   public projects$?: Observable<Project[]>;
@@ -29,21 +31,21 @@ export class WorkComponent {
   private getProjects$(filter: 'all' | 'active' | 'inactive'): Observable<Project[]> {
     const queryConstraints: QueryConstraint[] = [
       /** filter out private projects */
-      where('visibility', '==', 'public'),
+      where('visibility', '==', ProjectVisibility.PUBLIC),
     ];
 
     switch (filter) {
       case "all":
         /** filter out drafts */
-        queryConstraints.push(where('status', '!=', 'draft'))
+        queryConstraints.push(where('status', '!=', ProjectStatus.DRAFT))
         break;
       case "active":
         /** only show published projects */
-        queryConstraints.push(where('status', '==', 'published'))
+        queryConstraints.push(where('status', '==', ProjectStatus.PUBLISHED))
         break;
       case "inactive":
         /** only show archived projects */
-        queryConstraints.push(where('status', '==', 'archived'))
+        queryConstraints.push(where('status', '==', ProjectStatus.ARCHIVED))
         break;
       default:
         this.cLog.warn(`Something went wrong filtering projects`, filter);
