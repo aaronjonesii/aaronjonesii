eb48702da871584f541e72e0c1746f7a30ff7606
Anonymous One <jonesaa5@msu.edu>
2023-02-24 08:15:22 -0500

    chore: update animations

--- src/app/shared/components/navigation-bar/navigation-bar.animations.ts
+++ src/app/shared/components/navigation-bar/navigation-bar.animations.ts
@@ -4,9 +4,12 @@ export const NavigationBarAnimation = [
   trigger('navigationBarAnimation', [
     transition(':enter', [
       style({transform: 'translateY(100%)'}),
+      /** Emphasized decelerate */
       animate('400ms 100ms cubic-bezier(0.05, 0.7, 0.1, 1.0)', style({transform: 'translateY(0%)'})),
     ]),
     transition(':leave', [
+      style({transform: 'translateY(0%)'}),
+      /** Emphasized accelerate */
       animate('200ms 100ms cubic-bezier(0.3, 0.0, 0.8, 0.15)', style({transform: 'translateY(100%)'})),
     ]),
   ]),
