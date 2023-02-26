cd078982f424ee0cf860bd7498107365f59e0247
Anonymous One <jonesaa5@msu.edu>
2023-02-26 00:51:50 -0500

    chore: remove material sidenav

--- src/app/shared/components/layout/layout.module.ts
+++ src/app/shared/components/layout/layout.module.ts
@@ -7,12 +7,10 @@ import { RouterModule } from "@angular/router";
 import { MatButtonModule } from "@angular/material/button";
 import { MatIconModule } from "@angular/material/icon";
 import { SharedModule } from "../../shared.module";
-import { MatSidenavModule } from "@angular/material/sidenav";
 import { NavigationRailModule } from "../navigation-rail/navigation-rail.module";
 
 const ANGULAR_MATERIAL_MODULES = [
   MatToolbarModule, MatButtonModule, MatIconModule,
-  MatSidenavModule,
 ];
 
 const CORE_MODULES = [CommonModule, RouterModule];
