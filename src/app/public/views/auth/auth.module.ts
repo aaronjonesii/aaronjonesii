644636fe8cef7309f62169c6f7676228892e0d13
Anonymous One <jonesaa5@msu.edu>
2023-02-28 16:55:54 -0500

    chore: remove unused auth top app bar

--- src/app/public/views/auth/auth.module.ts
+++ src/app/public/views/auth/auth.module.ts
@@ -10,10 +10,9 @@ import { MatFormFieldModule } from "@angular/material/form-field";
 import { MatInputModule } from "@angular/material/input";
 import { MatToolbarModule } from "@angular/material/toolbar";
 import { MatIconModule } from "@angular/material/icon";
-import { AuthTopAppBarComponent } from './components/auth-top-app-bar/auth-top-app-bar.component';
 
 const COMPONENTS = [
-  AuthTopAppBarComponent,SignInComponent,
+  SignInComponent,
   SignUpComponent, ForgotPasswordComponent
 ];
 
