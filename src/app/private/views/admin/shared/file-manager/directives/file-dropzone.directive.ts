7f564934caa6c3f713db74a5789c0ed58b67b223
Aaron Jones II <jonesaaron1996@gmail.com>
2024-04-05 13:49:01 -0400

    refactor(file-dropzone-directive): standalone directive

--- src/app/private/views/admin/shared/file-manager/directives/file-dropzone.directive.ts
+++ src/app/private/views/admin/shared/file-manager/directives/file-dropzone.directive.ts
@@ -1,29 +1,32 @@
 import { Directive, ElementRef, EventEmitter, HostListener, Input, Output } from '@angular/core';
 
-@Directive({ selector: '[ajFileDropzone]' })
+@Directive({
+  selector: '[ajFileDropzone]',
+  standalone: true,
+})
 export class FileDropzoneDirective {
-  @Output() public dropped = new EventEmitter<FileList>();
-  @Output() public hovered = new EventEmitter<boolean>();
+  @Output() readonly dropped = new EventEmitter<FileList>();
+  @Output() readonly hovered = new EventEmitter<boolean>();
   @Input() hoverClass = 'hovered';
 
   constructor(private elementRef: ElementRef) {}
 
   @HostListener('drop', ['$event'])
-  public onDrop($event: DragEvent) {
+  onDrop($event: DragEvent) {
     $event.preventDefault();
     this.dropped.emit($event.dataTransfer?.files);
     this.onDragLeave($event);
   }
 
   @HostListener('dragover', ['$event'])
-  public onDragOver($event: DragEvent) {
+  onDragOver($event: DragEvent) {
     $event.preventDefault();
     this.elementRef.nativeElement.classList.add(this.hoverClass);
     this.hovered.emit(true);
   }
 
   @HostListener('dragleave', ['$event'])
-  public onDragLeave($event: DragEvent) {
+  onDragLeave($event: DragEvent) {
     $event.preventDefault();
     this.elementRef.nativeElement.classList.remove(this.hoverClass);
     this.hovered.emit(false);
