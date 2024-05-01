63b34eca3a1d61c57f07b1ec9317982d2c74b82d
Aaron Jones II <jonesaaron1996@gmail.com>
2024-04-30 22:46:26 -0400

    chore(admin): responsive navigation (#36)

--- src/app/features/admin/admin.component.ts
+++ src/app/features/admin/admin.component.ts
@@ -3,18 +3,60 @@ import { RouterOutlet } from "@angular/router";
 import { NavigationBarComponent } from "../../shared/components/navigation-bar/navigation-bar.component";
 import { GenericItem } from "../../shared/interfaces/generic-item";
 import { MenuService } from "../../shared/services/menu.service";
+import { BreakpointObserver } from "@angular/cdk/layout";
+import { map } from "rxjs";
+import { AsyncPipe } from "@angular/common";
+import {
+  NavigationDrawerComponent
+} from "../../shared/components/navigation-drawer/navigation-drawer.component";
+import {
+  NavigationRailComponent
+} from "../../shared/components/navigation-rail/navigation-rail.component";
+import {
+  TopAppBarComponent
+} from "../../shared/components/top-app-bar/top-app-bar.component";
+import {
+  NavigationRailAnimation
+} from "../../shared/components/navigation-rail/navigation-rail.animations";
+import {
+  NavigationBarAnimation
+} from "../../shared/components/navigation-bar/navigation-bar.animations";
+import {
+  NavigationDrawerAnimation
+} from "../../shared/components/navigation-drawer/navigation-drawer.animations";
 
 @Component({
   selector: 'aj-admin',
   templateUrl: './admin.component.html',
   styleUrl: './admin.component.scss',
   standalone: true,
-  imports: [RouterOutlet, NavigationBarComponent],
+  imports: [
+    RouterOutlet,
+    NavigationBarComponent,
+    AsyncPipe,
+    NavigationDrawerComponent,
+    NavigationRailComponent,
+    TopAppBarComponent,
+  ],
+  animations: [
+    NavigationRailAnimation,
+    NavigationBarAnimation,
+    NavigationDrawerAnimation,
+  ],
 })
 export class AdminComponent {
   readonly title = 'Admin';
-
   readonly segments: GenericItem[] = this.menuService.adminNavigationBarMenu;
+  /** Breakpoints can be found from src/assets/scss/partials/_media_queries.scss */
+  isMobile$ = this.breakpointObserver.observe('(max-width: 599px)')
+    .pipe(map(state => state.matches));
+  isTabletPortrait$ = this.breakpointObserver.observe('(min-width: 600px)')
+    .pipe(map(state => state.matches));
+  isDesktopExpanded$ = this.breakpointObserver.observe('(min-width: 1648px)')
+    .pipe(map(state => state.matches));
 
-  constructor(private menuService: MenuService) {}
+  constructor(
+    private menuService: MenuService,
+    private breakpointObserver: BreakpointObserver,
+  ) {}
 }
