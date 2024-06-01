73e59d2149afebb3dae1f6cc32eddd453ed40e59
Aaron Jones II <jonesaaron1996@gmail.com>
2024-05-31 21:59:19 -0400

    chore(scripts): add emojis to deploy production shell

--- scripts/deployProduction.sh
+++ scripts/deployProduction.sh
@@ -14,38 +14,38 @@ read -p "Input Firebase deployment message: " DEPLOYMENT_MESSAGE
 
 # Build Application
 npm run build:production || {
-  echo "Failed to build production application"
+  echo "‼️ Failed to build production application"
   exit 1
 }
 
 # Set Google Cloud Project
 gcloud config set project "$GCP_PROJECT" || {
-  echo "Failed to set Google Cloud Project to $GCP_PROJECT"
+  echo "‼️ Failed to set Google Cloud Project to $GCP_PROJECT"
   exit 1
 }
 
 # Build container image with Google Cloud Run
 gcloud builds submit --tag "gcr.io/$GCP_PROJECT/$GCP_SERVICE_NAME" || {
-  echo "Failed to build container image"
+  echo "‼️ Failed to build container image"
   exit 1
 }
 
 # Deploy container image to Google Cloud Run
 gcloud run deploy "$GCP_SERVICE_NAME" --image "gcr.io/$GCP_PROJECT/$GCP_SERVICE_NAME" --region "$GCP_REGION_NAME" || {
-  echo "Failed to deploy container image"
+  echo "‼️ Failed to deploy container image"
   exit 1
 }
 
 # Set Firebase project
 firebase use "$FIREBASE_PROJECT" || {
-  echo "Failed to set Firebase project to $FIREBASE_PROJECT"
+  echo "‼️ Failed to set Firebase project to $FIREBASE_PROJECT"
   exit 1
 }
 
 # Deploy Firebase Hosting configuration
 firebase deploy --only hosting -m "$DEPLOYMENT_MESSAGE" || {
-  echo "Failed to deploy Firebase Hosting"
+  echo "‼️ Failed to deploy Firebase Hosting"
   exit 1
 }
 
-echo "Finished deploying production build (0_o)"
+echo "✅ Finished deploying production build (0_o)"
