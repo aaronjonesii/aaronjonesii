1c24f4f5e9d44d13f4013fb1cc421628d5e310fc
Aaron Jones II <jonesaaron1996@gmail.com>
2024-06-03 23:00:32 -0400

    chore(book-appointment): update top app bar and seo

--- src/app/features/book-appointment/book-appointment.component.ts
+++ src/app/features/book-appointment/book-appointment.component.ts
@@ -1,4 +1,9 @@
 import { ChangeDetectionStrategy, Component } from '@angular/core';
+import {
+  TopAppBarService,
+} from '../../shared/components/top-app-bar/top-app-bar.service';
+import { SeoService } from '../../shared/services/seo.service';
+import { navPath } from '../../app.routes';
 
 @Component({
   selector: 'aj-book-appointment',
@@ -7,4 +12,22 @@ import { ChangeDetectionStrategy, Component } from '@angular/core';
   changeDetection: ChangeDetectionStrategy.OnPush,
   standalone: true,
 })
-export class BookAppointmentComponent {}
+export class BookAppointmentComponent {
+  private readonly title = 'Book Appointment';
+
+  constructor(
+    private seoService: SeoService,
+    private topAppBarService: TopAppBarService,
+  ) {
+    this.topAppBarService.setOptions({
+      title: this.title,
+      showBackBtn: false,
+      loading: false,
+    });
+
+    this.seoService.generateTags({
+      title: this.title,
+      route: navPath.bookAppointment,
+    });
+  }
+}
