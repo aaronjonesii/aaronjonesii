f5dc4d445b9d236628d32b30a4197cfbd18bd91b
Aaron Jones II <jonesaaron1996@gmail.com>
2024-06-07 12:10:46 -0400

    chore(hobbies): add/update hobbies

--- src/app/shared/services/hobbies.service.ts
+++ src/app/shared/services/hobbies.service.ts
@@ -6,11 +6,14 @@ import { toObservable } from '@angular/core/rxjs-interop';
 export class HobbiesService {
   protected readonly mockHobbies = signal<GenericItem[]>([
     { id: 'music', name: 'Music', icon: 'music_note' },
-    { id: 'fashion', name: 'Fashion', icon: 'styler' },
+    { id: 'fashion', name: 'Fashion', icon: 'interests' },
     { id: 'video-games', name: 'Video Games', icon: 'videogame_asset' },
     { id: 'travel', name: 'Traveling', icon: 'flight' },
-    { id: 'photo', name: 'Capturing Moments', icon: 'photo_camera' },
-    { id: 'diy', name: 'DIY', icon: 'build_circle' },
+    { id: 'photo', name: 'Capturing Moments', icon: 'party_mode' },
+    { id: 'diy', name: 'DIY', icon: 'handyman' },
+    { id: 'development', name: 'Software Development', icon: 'logo_dev' },
+    { id: 'campfires', name: 'Campfires', icon: 'local_fire_department' },
+    { id: 'sleeping', name: 'Sleeping', icon: 'single_bed' },
   ]);
 
   get getHobbies$() {
