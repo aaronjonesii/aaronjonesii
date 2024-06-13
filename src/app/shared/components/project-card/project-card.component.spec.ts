f174610e92b559f0b06ab09485fa42408158e648
Aaron Jones II <jonesaaron1996@gmail.com>
2024-06-13 15:21:06 -0400

    chore: lint for ci

--- src/app/shared/components/project-card/project-card.component.spec.ts
+++ src/app/shared/components/project-card/project-card.component.spec.ts
@@ -1,5 +1,6 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { ProjectCardComponent } from './project-card.component';
+import { ActivatedRoute } from "@angular/router";
 
 describe('ProjectCardComponent', () => {
   let component: ProjectCardComponent;
@@ -8,6 +9,12 @@ describe('ProjectCardComponent', () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       imports: [ProjectCardComponent],
+      providers: [
+        {
+          provide: ActivatedRoute,
+          useValue: {},
+        },
+      ],
     }).compileComponents();
 
     fixture = TestBed.createComponent(ProjectCardComponent);
