cdb0b7717fa0ea21cf08781d67f44f10079f1f86
Aaron Jones II <jonesaaron1996@gmail.com>
2024-06-18 12:03:10 -0400

    fix: scroll to top on navigation

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,11 @@
-import { Component } from '@angular/core';
+import { Component, Inject, OnDestroy } from '@angular/core';
 import { TopAppBarService } from "./shared/components/top-app-bar/top-app-bar.service";
 import { appInformation } from "./information";
-import { RouterOutlet } from "@angular/router";
+import { NavigationEnd, Router, RouterOutlet } from "@angular/router";
 import { SwUpdateService } from "./shared/services/sw-update.service";
 import { IconService } from "./shared/services/icon.service";
+import { filter, Subscription } from "rxjs";
+import { DOCUMENT } from "@angular/common";
 
 @Component({
   selector: 'aj-root',
@@ -11,13 +13,16 @@ import { IconService } from "./shared/services/icon.service";
   standalone: true,
   imports: [RouterOutlet],
 })
-export class AppComponent {
+export class AppComponent implements OnDestroy {
   readonly title = appInformation.title;
+  private subscriptions = new Subscription();
 
   constructor(
+    private router: Router,
     private iconService: IconService,
     private swUpdate: SwUpdateService,
     private topAppBarService: TopAppBarService,
+    @Inject(DOCUMENT) private document: Document,
   ) {
     this.iconService.registerSvgIcons();
 
@@ -28,5 +33,27 @@ export class AppComponent {
     });
 
     this.swUpdate.checkForSwUpdate();
+
+    const navigationEndEvents = this.router.events.pipe(
+      filter((event) => event instanceof NavigationEnd),
+    );
+
+    this.subscriptions.add(
+      navigationEndEvents.subscribe(() => this.resetScrollPosition()),
+    );
+  }
+
+  resetScrollPosition() {
+    if (typeof this.document === 'object' && this.document) {
+      const mainContentEl = this.document
+        .querySelector('main#content');
+      if (mainContentEl) {
+        mainContentEl.scrollTop = 0;
+      }
+    }
+  }
+
+  ngOnDestroy() {
+    this.subscriptions.unsubscribe();
   }
 }
